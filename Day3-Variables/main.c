115c015149fb669c2b427128e4be9ee58f64be06
Amudala yashwitha Reddy <amudalayashwithareddy@gmail.com>
2026-05-07 19:44:17 +0530

    Create Day3-Variables/main.c

---
+++ Day3-Variables/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    float cgpa = 9.2;
+    char section = 'A';
+    
+    printf("CSE Topper Yashna:\n");
+    printf("Age: %d\n", age);
+    printf("CGPA: %.1f\n", cgpa);
+    printf("Section: %c\n", section);
+    
+    return 0;
+}
