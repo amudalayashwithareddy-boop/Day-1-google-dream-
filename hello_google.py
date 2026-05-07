96e6c57c2438c551ace47b4b6db197ca0d3ec354
amudalayashwithareddy-boop <amudalayashwithareddy@gmail.com>
2026-05-07 11:00:47 +0530

    Day 1 : first python code to Google

---
+++ hello_google.py
@@ -0,0 +1,13 @@
+# Day 1 at MBU Hostel - 6 May 2026
+# Target: Software Engineer @ Google Hyderabad 2030
+
+print("Hi Google, I'm Yashwitha from MBU")
+print("Day 1 of my 4 year journey starts NOW")
+print("See you at ITC Kohenur in 2030!")
+
+# My Promise:
+print("Roju 1 code GitHub lo pedatha")
+print("4 years taruvatha nee office lo kurchunta")
+
+# Progress: 1/1460 days done ✅
+# Laptop: MBU Lab | Phone: GitHub App
