b57a0f51bf0a30be8d251fd948622c3f5381d97c
Amudala yashwitha Reddy <amudalayashwithareddy@gmail.com>
2026-05-07 16:46:55 +0530

    Create day2.c

---
+++ day2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Day 2/1460 - Google Hyderabad Journey
+    printf("Day 2: Sisters consistency starts now\n");
+    printf("My Sister: @janitha-coding\n");
+    printf("My Goal: Google SDE at Hyderabad\n");
+    printf("Days left: 1458\n");
+    printf("Yashna and Janitha will crack it!\n");
+    return 0;
+}
